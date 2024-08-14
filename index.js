823d08ffc4175e781f8a64eb4c3d731f8889cf6e
butterdogg1467 <momink2600@gmail.com>
2024-08-13 22:55:43 -0400

    Created classes and created buildTree method

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 document.addEventListener('DOMContentLoaded', function(){
 
+    class Node {
+        constructor(data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+
+    class Tree {
+        constructor(arr){
+            this.root = this.buildTree(arr)
+        }
+
+        buildTree(arr) {
+
+            if (arr.length === 0){
+                return null
+            } else if (arr.length === 1) {
+                return new Node (arr[0])
+            } else {
+                let setArr = new Set(arr)
+                let setArrToArr = Array.from(setArr)
+                let sortedArr = setArrToArr.sort((a, b) => a-b)
+    
+                let start = sortedArr[0]
+                let length = sortedArr.length
+                let end = length
+                let middle = (0 + end) / 2
+                let root = Math.floor(middle)
+
+                let rootNode = new Node(sortedArr[root])
+    
+                let leftHalf = sortedArr.slice(0, root)
+                let rightHalf = sortedArr.slice(root + 1, end)
+    
+                // console.log({sortedArr})
+                // console.log({leftHalf})
+                // console.log({rightHalf})
+                // console.log({root})
+                // console.log({rootNode})
+
+                rootNode.left = this.buildTree(leftHalf)
+                rootNode.right = this.buildTree(rightHalf)
+
+                return rootNode
+            }
+        }
+    }
+
+    let test = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 
 
 
-
-
-
-
-
-
-
-
-
+    
 
 
 
